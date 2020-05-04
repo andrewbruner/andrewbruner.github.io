31d78a27986c2d93f0a8c7c1b72c90e6396a214d
Andrew Bruner <andrewbruner@gmail.com>
2020-05-04 11:15:09 -0400

    Add Bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # andrewbruner.github.io
 My Github Page
+
+###`python -m http.server 3000`
--- script.js
+++ script.js
@@ -1,4 +1,40 @@
+const NavBar = () => (
+  <nav className="navbar navbar-expand-sm navbar-light bg-light">
+    <a className="navbar-brand" href="/">Andrew Bruner</a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ml-auto">  
+        <li className="nav-item">
+          <a className="nav-link" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Projects</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+const App = () => (
+  <div>
+    <NavBar />
+  </div>
+);
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.querySelector('.container')
+  <App />,
+  document.querySelector('.root')
 );
