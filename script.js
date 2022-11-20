e77b868244ab8c2298aca7191cfdabfcbc4f1b3f
Andrew Bruner <andrewbruner@gmail.com>
2022-11-20 02:52:46 +0000

    Update spoiler functionality

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-const statuses = document.querySelector('.statuses');
-const heading = document.createElement('h3');
-heading.textContent = 'Latest Posts';
-statuses.append(heading);
+function toggleSpoiler(event) {
+	const textContent = event.target.textContent;
+	event.target.textContent = textContent === 'Show More' ? 'Show Less' : 'Show More';
+	event.target.nextSibling.classList.toggle('hidden');
+}
 
 const url = 'https://fosstodon.org/api/v1/accounts/109339531040776096/statuses';
 
 fetch(url)
 	.then(response => response.json())
 	.then(data => {
-		data.forEach(datum => {
-			console.log(datum);
-			if (!datum.in_reply_to_account_id) {
+		const statuses = document.querySelector('.statuses');
+		let totalStatuses = 0;
+		for (let i = 0; i < data.length; i++) {
+			if (!data[i].in_reply_to_account_id) {
 				const status = document.createElement('div');
 				status.classList.add('status');
 				const date = document.createElement('h4');
 				date.classList.add('status-date');
 				const link = document.createElement('a');
-				link.href = datum.url;
-				const locales = 'en-US';
-				const options = { dateStyle: 'medium' };
-				link.textContent = new Date(datum.created_at).toLocaleDateString(locales, options);
+				link.href = data[i].url;
+				link.textContent = new Date(data[i].created_at)
+					.toLocaleDateString('en-US', { dateStyle: 'medium' });
 				date.append(link);
 				status.append(date);
 				const content = document.createElement('div');
 				content.classList.add('status-content');
 				let html = '';
-				if (datum.spoiler_text) {
-					html += `<p>${datum.spoiler_text}</p>`;
+				if (data[i].spoiler_text) {
+					html += `<p>${data[i].spoiler_text}</p><button class="spoiler-button" onclick="toggleSpoiler(event)">Show More</button><div class="hidden">${data[i].content}</div>`;
+				} else {
+					html += data[i].content;
 				}
-				html += datum.content;
-				if (datum.media_attachments.length) {
-					datum.media_attachments.forEach(media => {
+				if (data[i].media_attachments.length) {
+					data[i].media_attachments.forEach(media => {
 						html += `<img src="${media.url}" width="100%" />`;
 					});
 				}
 				content.innerHTML = html;
 				status.append(content);
 				statuses.append(status);
+				totalStatuses++;
 			}
-		});
+			if (totalStatuses > 4) {
+				break;
+			}
+		};
 	});
